6447f1324aaa1cfa88e22fcfdd3e6110bbcb3f9b
Owen <myartame@gmail.com>
2020-04-18 19:13:07 +0900

    .

--- environments/main/__kubernetes__.tf
+++ environments/main/__kubernetes__.tf
@@ -1,7 +1,7 @@
 
-module "kubernetes" {
-  source = "../../modules/kubernetes"
-
-  eks_cluster_name        = var.cluster_name
-  eks_worker_iam_role_arn = module.eks.eks_worker_iam_role_arn
-}
+# module "kubernetes" {
+#   source = "../../modules/kubernetes"
+#
+#   eks_cluster_name        = var.cluster_name
+#   eks_worker_iam_role_arn = module.eks.eks_worker_iam_role_arn
+# }
